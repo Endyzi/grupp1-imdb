7a145cea74322ce6a311b892ed7866ba87213f10
Kentjesus <private_nilhamn@hotmail.com>
2018-03-14 15:48:09 +0100

    skapat ratings table

---
+++ database/migrations/2018_03_14_144422_create_ratings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('movie_id');
+            $table->integer('user_id');
+            $table->integer('rating_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+}
