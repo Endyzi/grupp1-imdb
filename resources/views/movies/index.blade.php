e455ef0eb96a7ce807b619d585138af67c90ef36
Unknown <dr_styv@hotmail.com>
2018-03-16 15:50:20 +0100

    alex för fan....

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,5 +1,5 @@
 <h1>List of all movies in DB</h1>
 
 @foreach ($movies as $movie)
-    <p>{{ $movie->title }} - {{ $movie->releasedate }}</p>
+    <p>{{ $movie->titel }} - {{ $movie->releasedate }}</p>
 @endforeach
