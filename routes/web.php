cfc4dd2ec4185451abde73eae032c8f19eeedf77
Unknown <dr_styv@hotmail.com>
2018-03-16 15:45:57 +0100

    Removed duplicate line in web.php

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,9 @@
 
 Auth::routes();
 
-Route::get('/movies', 'MovieController@index')->name('movies.index');
-
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
