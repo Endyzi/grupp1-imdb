437247e90f90fd496c63c40bece909a651932256
Kentjesus <private_nilhamn@hotmail.com>
2018-03-16 14:04:41 +0100

    ändrade rubrik på sidan till imdb

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    IMDB
                 </div>
 
                 <div class="links">
