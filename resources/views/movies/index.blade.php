97241105613084a9dfc7b5a101ea4158896f9d6a
Kentjesus <private_nilhamn@hotmail.com>
2018-03-21 11:36:35 +0100

    added Runtime and plot to movies

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -11,7 +11,10 @@
 		  <img class="card-img-top" src="<?php if(strlen($movie->cover_url) > 0) echo $movie->cover_url ?>" alt="{{ $movie->titel }} Cover">
 		  <div class="card-body">
 		    <h5 class="card-title"><?php echo $movie->titel . " <br><br> " . $movie->releasedate; if($movie->releasedate > date('Y-m-d')) {echo "(Upcomming)";} ?></h5>
-		    <p class="card-text">{{ $movie->description }}</p>
+
+		    <p> Runtime: </p><p class="card-text">{{ $movie->length }}</p>
+
+		    <p> Plot:</p><p class="card-text">{{ $movie->description }}</p>
 		    <a class="btn btn-primary" href="{{ route('movies.show', ['movie' => $movie->id]) }}">More Info</a>
 		  </div>
 		</div>
